3799c4ef0e65f5691e61324ae0680bcc673d3c01
Carsten Bach <mail@carsten-bach.de>
2023-09-02 00:09:52 +0200

    Moved site-fns into correct site-id folder

--- mu-plugins/mu-per-blog/16/namespace.php
+++ mu-plugins/mu-per-blog/16/namespace.php
@@ -18,9 +18,11 @@ to the PT archive for 'ft_theme's
  */
 add_action( 'plugins_loaded', function (){
 
-	if ( '/demos' === \untrailingslashit( $_SERVER['REQUEST_URI'] ) )
-		if ( \wp_safe_redirect( '/themes/', 302, 'WordPress' ) )
+	if ( '/demos' === \untrailingslashit( $_SERVER['REQUEST_URI'] ) ) {
+		if ( \wp_safe_redirect( '/themes/', 302, 'WordPress' ) ) {
 			exit;
+		}
+	}
 
 	add_action( 'template_redirect', __NAMESPACE__ . '\\re_add_noblogredirect', -1 );
 }, 0 );
